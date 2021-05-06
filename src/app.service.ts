25407ddad394499c1be36bf691e9b3792c1b2354
andrewcs0901 <andrewcs0901@gmail.com>
2021-05-06 02:11:21 -0300

    feat: repo body on response

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,24 +2,24 @@ import { HttpService, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService) { }
 
   getRepos(language = `C#`): any {
     const repos = this.httpService.get(
       `https://api.github.com/users/takenet/repos`,
     );
-    return repos.toPromise().then((res) =>
-      res.data
-        ?.map((repo) => ({
-          description: repo.description,
-          full_name: repo.full_name,
-          language: repo.language,
-          created_at: repo.created_at,
-        }))
-        .filter((repo) => repo.language === language)
-        .sort((repoA, repoB) =>
-          repoA.created_at <= repoB.created_at ? -1 : 1,
-        ),
-    );
+    return {
+      repos: repos.toPromise().then((res) =>
+        res.data
+          ?.map((repo) => ({
+            description: repo.description,
+            full_name: repo.full_name,
+            language: repo.language,
+            created_at: repo.created_at,
+          }))
+          .filter((repo) => repo.language === language)
+          .sort((repoA, repoB) =>
+            repoA.created_at <= repoB.created_at ? -1 : 1,
+          ))
+    }
   }
-}
